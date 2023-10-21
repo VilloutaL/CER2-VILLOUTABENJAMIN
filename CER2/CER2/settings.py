f32c2333ef9e09af373d5f284eefa64bd385bc94
VilloutaL <benjamin.villouta@usm.cl>
2023-10-21 19:41:40 -0300

    Configuracion lista

--- CER2/CER2/settings.py
+++ CER2/CER2/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'certamen2',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

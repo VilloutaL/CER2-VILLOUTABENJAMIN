27569c8dcc61392805901c6eaaf2269cb4ea09bc
VilloutaL <benjamin.villouta@usm.cl>
2023-10-24 00:54:50 -0300

    Certamen casi terminado

--- CER2/certamen2/models.py
+++ CER2/certamen2/models.py
@@ -1,25 +1,29 @@
+from typing import Any
 from django.db import models
-
 # Create your models here.
 class Entidad(models.Model):
     id = models.BigAutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
-    logo = models.ImageField()
+    logo = models.ImageField(upload_to='photos')
 
+    def __str__(self) -> str:
+        return self.nombre
 
 class Comunicado(models.Model):
     id = models.BigAutoField(primary_key=True)
     titulo = models.CharField(max_length=50)
-    detalle = models.CharField(max_length=100)
-    detalle_corto = models.CharField(max_length=30)
-    TIPO_CHOICES =[
+    detalle = models.CharField(max_length=200)
+    detalle_corto = models.CharField(max_length=100)
+    TIPO_CHOICES =(
         ("S","Suspension de actividades"),
         ("C","Suspension de clase"),
         ("I","informacion")
-    ]   
-    tipo = TIPO_CHOICES
+    )
+    tipo = models.CharField(max_length=1, choices = TIPO_CHOICES)
     entidad =  models.ForeignKey(Entidad, on_delete=models.CASCADE)
     visible = models.BooleanField()
     fecha_publicacion = models.DateTimeField()
     fecha_ultima_modificacion = models.DateTimeField()
     
+    def __str__(self) -> str:
+        return self.titulo
--- CER2/certamen2/views.py
+++ CER2/certamen2/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Entidad, Comunicado
-
 # Create your views here.
 def index(request):
-    return render(request,'certamen2/index.html')
+    comunicados = Comunicado.objects.filter(visible = True)
+    if request.method == 'POST':
+        entidad_seleccionada = request.POST.get('select_entidad')
+        if entidad_seleccionada:
+            entidad = Entidad.objects.get(id = entidad_seleccionada)
+            comunicados = Comunicado.objects.filter(visible = True).filter(entidad__nombre = entidad)     
+    data = {
+        "comunicados": comunicados,
+        }
+    return render(request,'certamen2/index.html',data)
